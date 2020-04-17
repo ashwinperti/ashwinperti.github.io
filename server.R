227a246e08af62ff76b5a33eaf386dfc5d5885ab
Ashwin Perti <ashwinperti@abes.ac.in>
2020-04-17 12:08:04 +0530

    Initial commit

---
+++ server.R
@@ -0,0 +1,12 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+#
+
+library(shiny)
+
+library(shiny)
+server <- function(input, output) {
+    # empty
+}
